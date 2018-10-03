d4787ff84600c57144b3af042f59b2d7457d1901
Pranav Raikote <43779710+pranavraikote@users.noreply.github.com>
2018-10-03 23:24:35 +0530

    Add files via upload

---
+++ Wallpaper.py
@@ -0,0 +1,34 @@
+#Wallpaper of the day
+
+#Import the modules 
+import ctypes
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+
+#Open the website
+res=requests.get('https://bingwallpaper.com/')
+print('Getting the image')
+
+#Assign a soup object for parsing
+soup=BeautifulSoup(res.text,'lxml')
+
+#Find the tag in which image is located
+image_box=soup.find('a',{'class':'cursor_zoom'})
+
+#Get the image
+image=image_box.find('img')
+
+#Get the url to download the image
+link=image['src']
+
+#Get the image file and save it in the present-working-directory
+urllib.request.urlretrieve(link,'new.jpg')
+print('Image downloaded')
+
+#Image path
+path='E:\\Projects\\Wallpaper\\new.jpg'
+
+#Set the wallpaper
+ctypes.windll.user32.SystemParametersInfoW(20,0,path,3)
+print('Wallpaper set succesfully')
